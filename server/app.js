28594575f98b7d34de47e67152268a839994a68e
znguye <dnguyen.jmblc@gmail.com>
2025-05-29 20:14:20 +0200

    "mongoose - adding student and cohort schema"

--- server/app.js
+++ server/app.js
@@ -3,6 +3,12 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const PORT = 5005;
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch(err => console.error("Error connecting to mongo", err));
 
 // STATIC DATA
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
@@ -12,7 +18,6 @@ const students = require("./data/students.json");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
-
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
